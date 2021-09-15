4d6efd691d2eed50d8a54904e3a016d75b57dac7
tamagoyaki <rittitarou@yahoo.co.jp>
2021-09-15 17:11:06 +0900

    UI改善  - ベルトの効果選択UIを改善

--- lib/app/widgets/pages/dialog/effect_type_select_dialog.dart
+++ lib/app/widgets/pages/dialog/effect_type_select_dialog.dart
@@ -3,27 +3,10 @@ import 'package:belmer/app/models/accessory_m.dart';
 import 'package:belmer/app/utils/importer.dart';
 import 'package:supercharged/supercharged.dart';
 
-class EffectTypeSelectDialog extends StatelessWidget {
+class EffectTypeSelectDialog extends StatefulWidget {
   final BeltM? _beltM;
   final EffectModel? _selectedEffect;
   final void Function(EffectModel effectModel)? _onSelect;
-
-  const EffectTypeSelectDialog({
-    Key? key,
-    BeltM? beltM,
-    void Function(EffectModel effectModel)? onSelect,
-    EffectModel? selectedEffect,
-  })  : _beltM = beltM,
-        _onSelect = onSelect,
-        _selectedEffect = selectedEffect,
-        super(key: key);
-
-  void _handleSelect(BuildContext context, EffectModel effectModel) {
-    hide(context);
-    if (_onSelect != null) {
-      _onSelect!(effectModel);
-    }
-  }
 
   static void show(
     BuildContext context, {
@@ -56,11 +39,49 @@ class EffectTypeSelectDialog extends StatelessWidget {
     )..show();
   }
 
+  const EffectTypeSelectDialog({
+    Key? key,
+    BeltM? beltM,
+    void Function(EffectModel effectModel)? onSelect,
+    EffectModel? selectedEffect,
+  })  : _beltM = beltM,
+        _onSelect = onSelect,
+        _selectedEffect = selectedEffect,
+        super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => _Widget();
+}
+
+class _Widget extends State<EffectTypeSelectDialog> {
+  final _globalKey = GlobalKey();
+
+  @override
+  void initState() {
+    super.initState();
+
+    // 既に選択済の効果が存在する場合は、表示箇所までスクロール
+    WidgetsBinding.instance?.addPostFrameCallback((timeStamp) async {
+      BuildContext? context = _globalKey.currentContext;
+      if (context != null) {
+        Scrollable.ensureVisible(context);
+      }
+    });
+  }
+
+  void _handleSelect(BuildContext context, EffectModel effectModel) {
+    hide(context);
+    if (widget._onSelect != null) {
+      widget._onSelect!(effectModel);
+    }
+  }
+
   static void hide(BuildContext context) => Navigator.pop(context);
 
   @override
   Widget build(BuildContext context) {
-    Map<String, Map<String, List<EffectModel>>>? effectsMap = _beltM?.effects
+    Map<String, Map<String, List<EffectModel>>>? effectsMap = widget
+        ._beltM?.effects
         .groupListsBy((element) => element.groupName)
         .entries
         .map((e) => MapEntry<String, Map<String, List<EffectModel>>>(
@@ -73,8 +94,7 @@ class EffectTypeSelectDialog extends StatelessWidget {
       return Container();
     }
 
-    // TODO レイアウトを変更する
-    // TODO 選択されている場合は、その選択肢の位置までスクロールする仕組みを作る
+    // TODO レイアウトを変更する？
     return Container(
       height: 600,
       child: SingleChildScrollView(
@@ -85,36 +105,39 @@ class EffectTypeSelectDialog extends StatelessWidget {
                   child: ExpansionTile(
                     title: Text(groupEntry.key),
                     initiallyExpanded:
-                        _selectedEffect?.groupName == groupEntry.key,
+                        widget._selectedEffect?.groupName == groupEntry.key,
                     collapsedTextColor: collapasedColor,
                     collapsedIconColor: collapasedColor,
-                    children: groupEntry.value.entries
-                        .map(
-                          (kindEntry) => Container(
-                            padding: EdgeInsets.only(left: 10),
-                            child: ExpansionTile(
-                              title: Text(kindEntry.key),
-                              initiallyExpanded:
-                                  _selectedEffect?.kindName == kindEntry.key,
-                              collapsedTextColor: collapasedColor,
-                              collapsedIconColor: collapasedColor,
-                              children: kindEntry.value
-                                  .map(
-                                    (effect) => Container(
-                                      padding: EdgeInsets.only(left: 20),
-                                      child: ListTile(
-                                        title: Text(effect.value),
-                                        leading: Icon(Icons.add),
-                                        onTap: () =>
-                                            _handleSelect(context, effect),
-                                      ),
+                    children: groupEntry.value.entries.map(
+                      (kindEntry) {
+                        bool isSelectedKind =
+                            widget._selectedEffect?.kindName == kindEntry.key;
+
+                        return Container(
+                          padding: EdgeInsets.only(left: 10),
+                          child: ExpansionTile(
+                            key: isSelectedKind ? _globalKey : null,
+                            title: Text(kindEntry.key),
+                            initiallyExpanded: isSelectedKind,
+                            collapsedTextColor: collapasedColor,
+                            collapsedIconColor: collapasedColor,
+                            children: kindEntry.value
+                                .map(
+                                  (effect) => Container(
+                                    padding: EdgeInsets.only(left: 20),
+                                    child: ListTile(
+                                      title: Text(effect.value),
+                                      leading: Icon(Icons.add),
+                                      onTap: () =>
+                                          _handleSelect(context, effect),
                                     ),
-                                  )
-                                  .toList(),
-                            ),
+                                  ),
+                                )
+                                .toList(),
                           ),
-                        )
-                        .toList(),
+                        );
+                      },
+                    ).toList(),
                   ),
                 ),
               )
