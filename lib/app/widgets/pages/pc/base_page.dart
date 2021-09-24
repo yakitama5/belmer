69f4472d3938337b78584ae042a568bbaa9e425f
tamagoyaki <rittitarou@yahoo.co.jp>
2021-09-24 15:49:16 +0900

    UI改善  - 画面サイズに合わせてハンバーガーメニューになるように修正

--- lib/app/widgets/pages/pc/base_page.dart
+++ lib/app/widgets/pages/pc/base_page.dart
@@ -37,36 +37,6 @@ class BasePage extends StatelessWidget {
           ),
         ),
       ),
-      // TODO Drawerとボタン共通化してえ・・・・
-      // endDrawer: Drawer(
-      //   child: ListView(
-      //     children: [
-      //       _DrawerItem(
-      //         title: "Home",
-      //         iconData: Icons.home,
-      //         onTap: () =>
-      //             context.read<PageViewBloc>().add(PageViewEventHome()),
-      //       ),
-      //       _DrawerItem(
-      //         title: "Search",
-      //         iconData: Icons.search,
-      //         onTap: () =>
-      //             context.read<PageViewBloc>().add(PageViewEventSearch()),
-      //       ),
-      //       _DrawerItem(
-      //         title: "Info",
-      //         iconData: Icons.info,
-      //         onTap: () => InfoDialog.show(context),
-      //       ),
-      //       _DrawerItem(
-      //         title: "Logout",
-      //         iconData: Icons.logout,
-      //         onTap: () => context.read<AuthBloc>().add(SignOut()),
-      //       ),
-      //     ],
-      //   ),
-      // ),
-      // drawerEdgeDragWidth: 0,
     );
   }
 }
@@ -107,82 +77,85 @@ class _Header extends StatelessWidget {
   Widget _buildHeaderRightItem(BuildContext context) {
     return BlocBuilder<AuthBloc, AuthState>(
       builder: (context, state) {
-        List<Widget> items = [];
+        List<_HeaderItem> headerItems;
 
         // 認証状態によってボタンを変更する
         if (state is AuthSuccess) {
-          items.addAll([
-            _buildHomeButton(context),
-            _buildSearchButton(context),
-            _buildInfoButton(context),
-            _buildLogoutButton(context),
-          ]);
+          headerItems = [
+            _HeaderItem(
+              "Home",
+              Icons.home,
+              () => context.read<PageViewBloc>().add(PageViewEventHome()),
+            ),
+            _HeaderItem(
+              "Search",
+              Icons.search,
+              () => context.read<PageViewBloc>().add(PageViewEventSearch()),
+            ),
+            _HeaderItem(
+              "Info",
+              Icons.info,
+              () => InfoDialog.show(context),
+            ),
+            _HeaderItem(
+              "Logout",
+              Icons.logout,
+              () => context.read<AuthBloc>().add(SignOut()),
+            ),
+          ];
         } else {
-          items.add(
-            _buildInfoButton(context),
-          );
+          headerItems = [
+            _HeaderItem(
+              "Info",
+              Icons.info,
+              () => InfoDialog.show(context),
+            )
+          ];
         }
 
-        return _buildRightHeaderAllItems(context, items);
-        // TODO: 画面幅に合わせて省略する
-        // return LayoutBuilder(builder: (context, constraiants) {
-        //   if (ResponsiveWidget.isSmallScreen(context)) {
-        //     return _buildRightHeaderMenu(context, items);
-        //   } else {
-        //     return _buildRightHeaderAllItems(context, items);
-        //   }
-        // });
+        return LayoutBuilder(builder: (context, constraiants) {
+          if (ResponsiveWidget.isSmallScreen(context)) {
+            return _buildRightHeaderMenu(context, headerItems);
+          } else {
+            return _buildRightHeaderAllItems(context, headerItems);
+          }
+        });
       },
     );
   }
 
-  Widget _buildRightHeaderAllItems(BuildContext context, List<Widget> items) {
+  Widget _buildRightHeaderAllItems(
+      BuildContext context, List<_HeaderItem> items) {
     return Row(
       mainAxisAlignment: MainAxisAlignment.end,
-      children:
-          items.map((e) => [e, SpaceBox(width: 20)]).expand((e) => e).toList(),
+      children: items
+          .map((item) => [
+                HeaderButton(
+                    iconData: item.iconData,
+                    label: item.label,
+                    onTap: item.onTap),
+                SpaceBox(width: 20)
+              ])
+          .expand((e) => e)
+          .toList(),
     );
   }
 
-  Widget _buildRightHeaderMenu(BuildContext context, List<Widget> items) {
-    return TooltipIconButton(
-      icon: Icon(Icons.menu),
-      tooltipMessage: "Menu",
-      onTap: () => Scaffold.of(context).openEndDrawer(),
-    );
-  }
-
-  Widget _buildHomeButton(BuildContext context) {
-    return HeaderButton(
-      iconData: Icons.home,
-      label: "Home",
-      onTap: () => context.read<PageViewBloc>().add(PageViewEventHome()),
-    );
-  }
-
-  Widget _buildSearchButton(BuildContext context) {
-    return HeaderButton(
-      iconData: Icons.search,
-      label: "Search",
-      onTap: () => context.read<PageViewBloc>().add(PageViewEventSearch()),
-    );
-  }
-
-  Widget _buildInfoButton(BuildContext context) {
-    return HeaderButton(
-      iconData: Icons.info,
-      label: "Info",
-      onTap: () => InfoDialog.show(context),
-      // onTap: () => context.read<PageViewBloc>().add(PageViewEventTest()),
-    );
-  }
-
-  Widget _buildLogoutButton(BuildContext context) {
-    return HeaderButton(
-      iconData: Icons.logout,
-      label: "Logout",
-      onTap: () => context.read<AuthBloc>().add(SignOut()),
-    );
+  Widget _buildRightHeaderMenu(BuildContext context, List<_HeaderItem> items) {
+    return PopupMenuButton(
+        icon: Icon(Icons.menu),
+        iconSize: 30,
+        itemBuilder: (BuildContext context) => items
+            .map(
+              (item) => PopupMenuItem(
+                child: Row(
+                  children: [Icon(item.iconData), Text(item.label)],
+                ),
+                onTap: item.onTap,
+                textStyle: Theme.of(context).textTheme.headline1,
+              ),
+            )
+            .toList());
   }
 }
 
@@ -253,46 +226,10 @@ class _TwitterIconButton extends StatelessWidget {
   }
 }
 
-class _DrawerItem extends StatelessWidget {
-  final IconData? iconData;
-  final String title;
-  final void Function()? onTap;
+class _HeaderItem {
+  final String label;
+  final IconData iconData;
+  final void Function() onTap;
 
-  const _DrawerItem({
-    Key? key,
-    this.iconData,
-    required this.title,
-    this.onTap,
-  }) : super(key: key);
-
-  void handleOnTap(BuildContext context) {
-    if (onTap != null) {
-      Navigator.pop(context);
-      onTap!();
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.only(bottom: 20),
-      child: ListTile(
-        title: Row(
-          children: [
-            if (iconData != null)
-              Icon(
-                this.iconData,
-                size: 30,
-              ),
-            SpaceBox(width: 10),
-            Text(
-              this.title,
-              style: Theme.of(context).textTheme.headline1,
-            ),
-          ],
-        ),
-        onTap: () => handleOnTap(context),
-      ),
-    );
-  }
+  _HeaderItem(this.label, this.iconData, this.onTap);
 }
